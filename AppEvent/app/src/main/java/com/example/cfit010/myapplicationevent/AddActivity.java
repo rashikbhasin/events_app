2b5c5648c4549e497df5c7281770bf50f5156281
rashikbhasin <rashikbhasin@gmail.com>
2016-07-27 10:30:15 +0530

    Date Format Added

--- AppEvent/app/src/main/java/com/example/cfit010/myapplicationevent/AddActivity.java
+++ AppEvent/app/src/main/java/com/example/cfit010/myapplicationevent/AddActivity.java
@@ -1,9 +1,13 @@
 package com.example.cfit010.myapplicationevent;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,7 +15,15 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class AddActivity extends AppCompatActivity {
+
+
+    static final int DATE_DIALOG_ID = 0;
+    private int mYear,mMonth,mDay;
+    EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +32,33 @@ public class AddActivity extends AppCompatActivity {
 
         Button addButton=(Button)findViewById(R.id.submit);
 
+        Log.d("Heree","At top");
+        Calendar c=Calendar.getInstance();
+        mYear=c.get(Calendar.YEAR);
+        mMonth=c.get(Calendar.MONTH);
+        mDay=c.get(Calendar.DAY_OF_MONTH);
+        //String dateFormat = "dd/MM/yyyy";
+        mEditText = (EditText) findViewById(R.id.add_event_date);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        mEditText.setText(sdf.format(c.getTime()));
+
+        mEditText.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                showDialog(DATE_DIALOG_ID);
+
+            }
+        });
+
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 JSONObject json=new JSONObject();
+                openDateActivity(view);
 
                 try
                 {
@@ -61,6 +95,74 @@ public class AddActivity extends AppCompatActivity {
                 postJson.execute(serverURL);
             }
         });
+    }
+    //                    json.put("date",date.getText().toString());
+//    Log.d("Heree",mDate);
+
+
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this,
+                        mDateSetListener,
+                        mYear, mMonth, mDay);
+
+        }
+
+        return null;
+
+    }
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            mYear = year;
+            mMonth = monthOfYear;
+            mDay = dayOfMonth;
+//            mEditText.setText(new StringBuilder().append(mDay).append("/").append(mMonth+1).append("/").append(mYear));
+            mEditText.setText(new StringBuilder().append(mYear).append("/").append(mMonth+1).append("/").append(mDay));
+
+        }
+
+    };
+
+    public void openDateActivity(View view) {
+        JSONObject obj = new JSONObject();
+        String month = "";
+        String day = "";
+        if(mMonth<10)
+        {
+            month = "0"+String.valueOf(mMonth+1);
+        }
+        else
+        {
+            month = String.valueOf(mMonth);
+        }
+        if(mDay<10)
+        {
+            day = "0"+String.valueOf(mDay);
+        }
+        else
+        {
+            day = String.valueOf(mDay);
+        }
+        String dateFormat = String.valueOf(mYear)+"-"+month+"-"+day;
+        EditText date=(EditText)findViewById(R.id.add_event_date);
+        date.setText(dateFormat);
+
+//          mDate = String.valueOf(mYear)+"-"+month+"-"+day;
+//        try {
+//
+//            obj.put("date", k-date);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        String method = getIntent().getStringExtra("method");
+//        TextView name=(TextView)findViewById(R.id.output);
+//        String url =  new HttpRequest().url;
+//        PostAsyncTask get_request = new PostAsyncTask(name,obj.toString());
+//        get_request.execute(url+"/"+method);
     }
 
 
--- AppEvent/app/src/main/java/com/example/cfit010/myapplicationevent/UpdateActivity.java
+++ AppEvent/app/src/main/java/com/example/cfit010/myapplicationevent/UpdateActivity.java
@@ -1,16 +1,23 @@
 package com.example.cfit010.myapplicationevent;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class UpdateActivity extends AppCompatActivity {
 
@@ -21,6 +28,7 @@ public class UpdateActivity extends AppCompatActivity {
 
         Intent intent=getIntent();
         final String received=intent.getStringExtra("data");
+
 
         JSONObject reader;
 
@@ -87,6 +95,4 @@ public class UpdateActivity extends AppCompatActivity {
             }
         });
     }
-
-
 }
